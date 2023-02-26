b80b3553fb855612ee80a526492fbe4c338072b7
SeanDuffie <sean.duffie@tamu.edu>
2023-02-26 13:54:48 -0600

    Captured Initial testing screenshots OpenCV can now see the screen Starting on text recognition

---
+++ main.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2
+from mss import mss
+from PIL import Image
+
+from screeninfo import get_monitors
+for m in get_monitors():
+    print(str(m))
+
+bounding_box = {'top': 800, 'left': 2160, 'width': 400, 'height': 600}
+
+sct = mss()
+
+while True:
+    sct_img = sct.grab(bounding_box)
+    cv2.imshow('screen', np.array(sct_img))
+
+    if (cv2.waitKey(1) & 0xFF) == ord('q'):
+        cv2.destroyAllWindows()
+        break
