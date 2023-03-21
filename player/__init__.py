c8ad046a231f67997325def274049d2170b2bda0
SeanDuffie <sean.duffie@tamu.edu>
2023-03-20 19:30:44 -0500

    Begins to integrate each component into a module

---
+++ player/__init__.py
@@ -0,0 +1,6 @@
+"""
+__init__.py
+"""
+from .player import init_cast, recast_fisher
+from .reader import ImgHandler
+from .screen import Screen
---
+++ player/main.py
@@ -0,0 +1,7 @@
+from player import *
+from reader import ImgHandler
+from screen import Screen
+
+
+if __name__ == "__main__":
+    
