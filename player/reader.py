f0911d0d8d7fbdd0a28be9de69060724c579028b
SeanDuffie <sean.duffie@tamu.edu>
2023-03-25 18:24:24 -0500

    Finished Text Reader

--- player/reader.py
+++ player/reader.py
@@ -1,25 +1,38 @@
-# Import required packages
+""" reader.py
+    This file will handle any reading of the image
+    The 
+"""
+import sys
+import logging
 import cv2
 import pytesseract
 
 class ImgHandler:
     """ Tracks and reads an image """
-    def __init__(self) -> None:
+    def __init__(self, img=None) -> None:
         # Mention the installed location of Tesseract-OCR in your system
         pytesseract.pytesseract.tesseract_cmd = r'/Program Files/Tesseract-OCR/tesseract.exe'
-        
+
         # Private Variables
-        self.img = None
+        if img is not None:
+            self.img = img
+            self.preprocess()
         self.contours = None
+        self.hierarchy = None
 
     def update_img(self, img):
+        """ Replace the current image being read """
         self.img = img
+        self.preprocess()
+
+    def show_img(self):
+        """ Show the current image being processed """
+        cv2.imshow("Screen Preview", self.img)
 
     def preprocess(self):
         """ Preprocessing the image starts
             TODO: add detail on process
         """
-
         # Convert the image to gray scale
         gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
 
@@ -37,26 +50,31 @@ class ImgHandler:
         dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1)
 
         # Finding contours
-        contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
+        self.contours, self.hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
                                                         cv2.CHAIN_APPROX_NONE)
-        
-    def read_text():
-        
+
+    def read_text(self):
+        """ Read the text contained within the bounding box """
+        # Check loaded image
+        if self.img is None:
+            logging.error("%s\t| No image loaded. Call update_Img() or init with img.", __name__)
+            sys.exit(1)
+
         # Looping through the identified contours
         # Then rectangular part is cropped and passed on
         # to pytesseract for extracting text from it
         # Extracted text is then written into the text file
-        for cnt in contours:
+        for cnt in self.contours:
             x, y, w, h = cv2.boundingRect(cnt)
 
             # Drawing a rectangle on copied image
+            im2 = self.img.copy()
             rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
             # Cropping the text block for giving input to OCR
             cropped = im2[y:y + h, x:x + w]
 
-            # Open the file in append mode
-            file = open("recognized.txt", "a")
-
             # Apply OCR on the cropped image
             text = pytesseract.image_to_string(cropped)
+
+        return text
