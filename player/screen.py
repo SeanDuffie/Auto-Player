fb833b252712fcd0ca8ec07d26968c96eb67dca5
SeanDuffie <sean.duffie@tamu.edu>
2023-03-25 18:21:54 -0500

    Finished Screen Functionality

--- player/screen.py
+++ player/screen.py
@@ -1,33 +1,47 @@
+""" screen.py
+    Provides a tool for grabbing frames from the screen
+"""
 import numpy as np
-import cv2
 from mss import mss
 
 from screeninfo import get_monitors
 
 class Screen:
-    """ Deals with Screen issues """
-    def __init__(self):
-        self.p_mon = get_monitors()[0]
-        w = 400
-        h = 600
+    """ Provides the ability to capture frames from the screen """
+    def __init__(self, mon=0):
+        self.p_mon = get_monitors()[mon]
+        self.width = 400
+        self.height = 600
+        self.box_top = self.p_mon.height - self.height - 100
+        self.box_left = self.p_mon.width - self.width
         self.sct = mss()
 
-    def select_monitor(self, mon=0):
+        self.bounding_box = {
+            'top': self.box_top,
+            'left': self.box_left,
+            'width': self.width,
+            'height': self.height
+        }
+
+
+
+    def change_monitor(self, mon):
         """ Select the monitor to grab images from """
         self.p_mon = get_monitors()[mon]
 
-    def select_box(self, top, left, width, height):
+    def change_box(self, top, left, width, height):
         """ Configures the box to read from """
-        bounding_box = {'top': top-height-100, 'left': left - width, 'width': width, 'height': height}
+        bounding_box = {
+            'top': top,
+            'left': left,
+            'width': width,
+            'height': height
+        }
+
         sct_img = self.sct.grab(bounding_box)
         return sct_img
 
-
-# while True:
-
-#     # TODO: Showing for debugging, not needed later
-#     cv2.imshow('screen', np.array(sct_img))
-
-#     if (cv2.waitKey(1) & 0xFF) == ord('q'):
-#         cv2.destroyAllWindows()
-#         break
+    def get_image(self):
+        """ Grabs the most recent screen frame """
+        sct_img = np.array(self.sct.grab(self.bounding_box))
+        return sct_img
