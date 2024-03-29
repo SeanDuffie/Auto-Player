a31735c243e874cf27fd3654fcc9534aef95119b
SeanDuffie <sean.duffie@tamu.edu>
2024-03-28 20:12:41 -0400

    Added logging for print timestamp formatting Re-added the sleep between keyboard writes Removed the winsound for delays

--- type_script.py
+++ type_script.py
@@ -1,30 +1,37 @@
-import time
+""" @file type_script.py
+    @author Sean Duffie
+    @brief Made purely to spite Ratoz
+"""
 import datetime
+import logging
+import time
+
 import keyboard
-import winsound
+
+# Initial Logger Settings
+FMT_MAIN = "%(asctime)s\t| %(levelname)s\t| %(message)s"
+logging.basicConfig(format=FMT_MAIN, level=logging.DEBUG, datefmt="%Y-%m-%d %H:%M:%S")
 
 for i in range(3):
-    print(f"Starting in {3-i}...")
-    winsound.PlaySound("*", winsound.SND_ALIAS)
+    logging.info("Starting in %d...", 3-i)
     time.sleep(1)
 
 last_time = datetime.datetime(2024, 3, 28, 18, 4)
 cur_time = datetime.datetime.now()
 keyboard.write("/bump\t")
-# time.sleep(.5)
+time.sleep(.5)
 keyboard.write("\n\n")
 
 while True:
     while cur_time - last_time < datetime.timedelta(hours=2):
         cur_time = datetime.datetime.now()
         remaining = datetime.timedelta(hours=2) - (cur_time - last_time)
-        if remaining.total_seconds() < 7:
-            winsound.PlaySound("*", winsound.SND_ALIAS)
         if (remaining.total_seconds() // 1) % 60 == 0:
-            print(f"Time Remaining = {remaining}")
+            logging.info("Time Remaining = %s", remaining)
 
         time.sleep(1)
 
     keyboard.write("/bump\t")
+    time.sleep(.5)
     keyboard.write("\n\n")
     last_time = datetime.datetime.now()
