378d5873af7972f579b8557e10af612e9b9cd5c0
Sean Duffie <duffiesean@gmail.com>
2024-03-30 00:31:35 -0400

    Switched to more intuitive, single loop

--- type_script.py
+++ type_script.py
@@ -12,34 +12,29 @@ import keyboard
 FMT_MAIN = "%(asctime)s\t| %(levelname)s\t| %(message)s"
 logging.basicConfig(format=FMT_MAIN, level=logging.DEBUG, datefmt="%Y-%m-%d %H:%M:%S")
 
-# for i in range(3):
-#     logging.info("Starting in %d...", 3-i)
-#     time.sleep(1)
-
-last_time = datetime.datetime(2024, 3, 29, 10, 38, 00)
+last_time = datetime.datetime(2024, 3, 29, 22, 38, 00)
 cur_time = datetime.datetime.now()
 interval = datetime.timedelta(hours=2, minutes=0)
-# interval = datetime.timedelta(minutes=1)
+# interval = datetime.timedelta(minutes=2)
 # interval = datetime.timedelta(seconds=5)
-# keyboard.write("/bump\t")
-# time.sleep(.5)
-# keyboard.write("\n\n")
 
 while True:
-    while cur_time - last_time < interval:
-        cur_time = datetime.datetime.now()
-        remaining = interval - (cur_time - last_time)
-        if (remaining.total_seconds()*10 // 10) % 60 == 0:
-            logging.info("Time Remaining = %s", remaining)
+    cur_time = datetime.datetime.now()
+    elapsed: datetime.timedelta = cur_time - last_time
+    remaining: datetime.timedelta = interval - elapsed
 
-        time.sleep(.1)
+    if (remaining.total_seconds()*10 // 10) % 60 == 0:
+        logging.info("Time Remaining = %s", remaining)
 
-    logging.info("Writing to Keyboard")
-    last_time = datetime.datetime.now()
-    count = 3
-    while count < 3:
-        keyboard.write("/bump\t")
-        time.sleep(.5)
-        keyboard.write("\n\n")
-        time.sleep(1)
-        count += 1
+    if remaining.total_seconds() <= 0:
+        logging.info("Writing to Keyboard")
+        last_time = datetime.datetime.now()
+        count = 0
+        while count < 3:
+            keyboard.write("/bump\t")
+            time.sleep(.5)
+            keyboard.write("\n\n")
+            time.sleep(1)
+            count += 1
+
+    time.sleep(1)
